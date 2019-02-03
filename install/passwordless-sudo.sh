adc55b1dc615c05df5ebfde8a725030054397d02
Marc Diethelm <mail@marcd.ch>
2019-02-03 02:42:13 +0100

    Only set up passwordless sudo once

--- install/passwordless-sudo.sh
+++ install/passwordless-sudo.sh
@@ -1,2 +1,14 @@
-sudo cp /etc/sudoers /etc/sudoers~
-printf "\n%s\n%s\n" "# Passwordless sudo for khoe user" "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+#!/usr/bin/env bash
+
+comment="# Passwordless sudo for khoe user"
+directive="$USER ALL=(ALL) NOPASSWD: ALL"
+
+if sudo grep -Fxq "$directive" /etc/sudoers
+then
+    printf "%s\n" "Passwordless sudo for khoe user is already set up."
+else
+    printf "%s\n" "Setting up passwordless sudo for khoe user."
+    sudo cp /etc/sudoers /etc/sudoers~
+    printf "\n%s\n%s\n" "$comment" "$directive" | sudo tee -a /etc/sudoers  > /dev/null
+    printf "%s\n" "Done"
+fi
