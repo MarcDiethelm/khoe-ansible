31e45f4b86b94d48615b03b352ae37e8104cb526
Marc Diethelm <mail@marcd.ch>
2019-07-26 13:48:09 +0200

    Install: make apt-get select the default options. Fixes #23

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -2,6 +2,7 @@
 
 sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install --yes \
     software-properties-common \
     ansible \
