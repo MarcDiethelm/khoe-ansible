3c995fc2318dcb949e16abea6f4c20826ef3dc85
Marc Diethelm <mail@marcd.ch>
2019-07-30 01:15:12 +0200

    passwordless-sudo.sh: now requires a username as argument

--- scripts/passwordless-sudo.sh
+++ scripts/passwordless-sudo.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-comment="# Passwordless sudo for khoe user"
-directive="$USER ALL=(ALL) NOPASSWD: ALL"
+comment="# Passwordless sudo for user $1"
+directive="$1 ALL=(ALL) NOPASSWD: ALL"
 
 if sudo grep -Fxq "$directive" /etc/sudoers
 then
-    printf "%s\n" "Passwordless sudo for khoe user is already set up."
+    printf "%s\n" "Passwordless sudo is already set up for: $1."
 else
-    printf "%s\n" "Setting up passwordless sudo for khoe user."
+    printf "%s\n" "Setting up passwordless sudo for user: $1."
     sudo cp /etc/sudoers /etc/sudoers~
     printf "\n%s\n%s\n" "$comment" "$directive" | sudo tee -a /etc/sudoers  > /dev/null
     printf "%s\n" "Done"
