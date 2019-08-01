fb33a9a30808dfeb4474152763314887f9610995
Marc Diethelm <mail@marcd.ch>
2019-08-01 20:33:49 +0200

    Smarter install, downloads latest release from Github
    - Split in `bootstrap.sh [no-autoinstall]` and install.sh
    - New install location: /usr/local/lib/khoe-nas/versions/[tag], symlinked to /usr/local/lib/khoe-nas/current

---
+++ bootstrap.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# This script is used to install Ansible and download the latest Khoe NAS on the server.
+
+set -eo pipefail
+
+install_base_path=/usr/local/lib/khoe-nas/versions
+
+printf '\n%s\n' "Installing Ansible"
+sudo apt-add-repository --yes ppa:ansible/ansible
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get install --yes \
+    software-properties-common \
+    ansible \
+    python-jmespath \
+	jq # for parsing versions.json
+
+
+# Dev: `bash bootstrap.sh no-autoinstall` exits the script here.
+# prevents downloading and installing latest published version
+if [ "$1" == "no-autoinstall" ]
+then
+	printf '\n%s\n' "No auto-install requested. Exiting."
+	exit 0
+fi
+
+# Github sorts the tags correctly (by tag not by date)
+# therefore we can use the first element from versions array
+# using https://stedolan.github.io/jq/
+versions=`curl https://api.github.com/repos/khoe-cloud/khoe-nas/tags`
+
+latest=`echo $versions      | jq '.[0] | .name'        | tr -d \"`
+tarball_url=`echo $versions | jq '.[0] | .tarball_url' | tr -d \"`
+version=$latest
+
+printf '\n%s\n' "Downloading khoe-nas: $version"
+install_path=$install_base_path/$version
+sudo mkdir -p $install_path
+
+# Wget download to stdout and pipe into untar, controlling the extracted dir name
+sudo bash -c "wget $tarball_url -O - | tar -xz --directory=$install_path --strip-components 1"
+printf '%s\n' "Saved to: $install_path"
+
+printf '\n%s\n' "Running $install_path/install.sh"
+. $install_path/install.sh
--- install.sh
+++ install.sh
@@ -3,21 +3,29 @@
 # This script is used to set up Ansible and dependencies on the server and is run on the server not on a development machine.
 # The idea is to do as little as possible, but as much as needed in this script.
 
-# ./ansible.cfg sets roles_path for ansible-galaxy installs
+# ./ansible.cfg sets roles_path for ansible-galaxy installs (first writeable directory in list)
+# ./ansible.cfg sets inventory path
 
-scriptpath=$(dirname $(realpath $0)) && \
-\
+scriptpath=$(dirname $(realpath $0))
+khoe_nas_path=/usr/local/lib/khoe-nas/current
+khoe_user='khoe'
+
+export KHOE_NAS_PATH=$khoe_nas_path
+
 printf '\n%s\n' "Running scripts/passwordless-sudo.sh." && \
-$scriptpath/scripts/passwordless-sudo.sh && \
+$scriptpath/scripts/passwordless-sudo.sh $khoe_user && \ # todo: do this with ansible
 \
-printf '\n%s\n' "Running scripts/install-ansible.sh." && \
-$scriptpath/scripts/install-ansible.sh && \
+sudo chown 700 $scriptpath/ansible.cfg && \
 \
 printf '\n%s\n' "Installing playbook dependencies from Ansible Galaxy." && \
 ansible-galaxy install -r requirements.yml && \
 \
-printf '\n%s\n' "Installing programs and config files." && \
+printf '\n%s\n' "Setting system, installing khoe-nas dependencies and config files." && \
 ansible-playbook $scriptpath/playbooks/install.yml && \
+\
+printf '\n%s\n' "Symlinking source: $scriptpath to link: $khoe_nas_path" && \
+sudo rm -f $khoe_nas_path && \
+sudo ln -s $scriptpath $khoe_nas_path && \
 \
 cat <<DOC
 Installation complete.
@@ -30,6 +38,3 @@ ansible-playbook playbooks/mount.yml -e task=create -e disk_label=label
 To create a user with a NAS share:
 ansible-playbook playbooks/user.yml -e task=create -e username=example1 -e "password='1234'" [-e share_name=username] [-e disk_label=label] [-e "smbpassword='2345'"]
 DOC
-
-# https://github.com/ansible/ansible-examples/blob/master/language_features/ansible_pull.yml
-# ansible-pull # need to check it out!
